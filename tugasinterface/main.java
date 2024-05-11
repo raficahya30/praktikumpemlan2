dc86c719523ca89b0db794d18b1b3235f648b3fe
raficahya30 <160980809+raficahya30@users.noreply.github.com>
2024-05-11 19:50:48 +0700

    tugas modul 8 interface

---
+++ tugasinterface/main.java
@@ -0,0 +1,20 @@
+package tugasinterface;
+
+public class main {
+    public static void main(String[] args) {
+    Invoice invoice1 = new Invoice("Sepatu", 1, 2000000);
+    Invoice invoice2 = new Invoice("Tas", 1, 500000);
+    Invoice[] invoices = {invoice1, invoice2};
+    Employee employee = new Employee(954023, "Rafi",5000000, invoices);
+        
+    System.out.println("--------------------------------------------");
+    System.out.println("INFORMASI KARYAWAN \n");
+    System.out.println(employee);
+    System.out.println("Total gaji setelah dipotong hutang belanjaan di koperasi : " + employee.getPayableAmount());
+    System.out.println("--------------------------------------------------------------------- ");
+    System.out.println("INVOICE KARYAWAN" );
+    employee.displayInvoices();
+    System.out.println("---------------------------------------------------------------------");
+    }
+}
+
